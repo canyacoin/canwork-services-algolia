1fe40839e5ed25cf9d9ca1e1f324f2a77dc54bf6
alsco77 <alsco77@pm.me>
2018-09-18 16:57:28 +1000

    Implemented randomisation of algolia results batch style

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"net/http"
 
 	"github.com/algolia/algoliasearch-client-go/algoliasearch"
@@ -11,18 +12,15 @@ import (
 )
 
 var (
-	client algoliasearch.Client
-	index  algoliasearch.Index
+	algoliaAppID  string
+	algoliaAPIKey string
+	algoliaIndex  string
 )
 
 func init() {
-
-	algoliaAppID := getEnv("ALGOLIA_APP_ID", "")
-	algoliaAPIKey := getEnv("ALGOLIA_API_KEY", "")
-	algoliaIndex := getEnv("ALGOLIA_INDEX", "")
-
-	client = algoliasearch.NewClient(algoliaAppID, algoliaAPIKey)
-	index = client.InitIndex(algoliaIndex)
+	algoliaAppID = getEnv("ALGOLIA_APP_ID", "")
+	algoliaAPIKey = getEnv("ALGOLIA_API_KEY", "")
+	algoliaIndex = getEnv("ALGOLIA_INDEX", "")
 
 	http.HandleFunc("/", handleRoot)
 }
@@ -34,6 +32,38 @@ func main() {
 func handleRoot(w http.ResponseWriter, r *http.Request) {
 	ctx := appengine.NewContext(r)
 
-	log.Infof(ctx, "")
-	fmt.Fprintln(w, fmt.Sprintf("%v", 2))
+	client := algoliasearch.NewClient(algoliaAppID, algoliaAPIKey)
+	index := client.InitIndex(algoliaIndex)
+
+	fmt.Fprintln(w, "Endpoint hit success")
+	log.Infof(ctx, "Getting index...")
+
+	it, err := index.BrowseAll(algoliasearch.Map{"query": ""})
+	if err != nil {
+		log.Errorf(ctx, "Error retrieving results from index")
+	}
+
+	objects := []algoliasearch.Object{}
+	var hit algoliasearch.Map
+	for {
+		if hit, err = it.Next(); err != nil {
+			if err == algoliasearch.NoMoreHitsErr {
+				log.Infof(ctx, "End of results")
+			} else {
+				log.Errorf(ctx, "Error while browsing results")
+			}
+			break
+		}
+		objectID := hit["objectID"]
+		log.Debugf(ctx, fmt.Sprintf("Res %v", objectID))
+		objects = append(objects, algoliasearch.Object{"objectID": objectID, "salt": rand.Uint32()})
+	}
+
+	log.Infof(ctx, fmt.Sprintf("Updating records: %d", len(objects)))
+	_, err = index.PartialUpdateObjects(objects)
+	if err != nil {
+		log.Criticalf(ctx, fmt.Sprintf("Failed to update objects"))
+	} else {
+		log.Infof(ctx, fmt.Sprintf("Updated records"))
+	}
 }
